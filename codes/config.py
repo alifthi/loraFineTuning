7d1b6c28f8016e6f705896e914db374795541dbb
alifthi <alifathi8008@gmail.com>
2024-04-19 12:37:01 +0000

    finetuning: loadData method developed

---
+++ codes/config.py
@@ -0,0 +1,2 @@
+MODEL_NAME='Qwen/Qwen1.5-0.5B-Chat'
+DATA_DIR='../data/alpaca'
--- codes/utils/utils.py
+++ codes/utils/utils.py
@@ -1,12 +1,13 @@
-from datasets import load_from_disk
+from datasets import load_from_disk, load_dataset
 from transformers import AutoTokenizer
+from config import MODEL_NAME, DATA_DIR
 class utils:
-    def __init__(self, modelName='Qwen/Qwen1.5-0.5B-Chat', dataDir='../data/alpaca') -> None:
-        self.dataDir=dataDir
-        self.modelName=modelName
+    def __init__(self) -> None:
+        self.dataDir=DATA_DIR
+        self.modelName=MODEL_NAME
         self.data=self.loadData()
-    def loadData(self):
-        return load_from_disk(self.dataDir)
+    def loadData(self,dataset=None):
+        return load_from_disk(self.dataDir) if isinstance(dataset,type(None)) else load_dataset(dataset)
     def preprocessing(self):
         self.tokenizer=AutoTokenizer.from_pretrained(self.modelName)
         
