b71b85ddd635c4807ed5f6c25cccc1c98b8b50a4
Alifthi <alifathi8008@gmail.com>
2024-04-20 14:43:03 +0330

    finetuning: convertToChatTemplate method developed to convert texts to chat template

--- codes/utils/utils.py
+++ codes/utils/utils.py
@@ -11,4 +11,13 @@ class utils:
         return load_from_disk(self.dataDir) if isinstance(dataset,type(None)) else load_dataset(dataset,split='train')
     def preprocessing(self):
         self.tokenizer=AutoTokenizer.from_pretrained(self.modelName)
-        
+        self.data=self.data.map(self.convertToChatTemplate)
+    def convertToChatTemplate(self,text):
+        text=text['text'].split('### Human:')[-1]
+        text=text.split('### Assistant:')
+        userContent=text[0]
+        assistantContent=text[1]
+        chat=[{'role':'user','content':userContent},
+              {'role':'assistant','content':assistantContent}]
+        chat=self.tokenizer.apply_chat_template(chat,tokenize=False,add_generation_prompt=True)
+        return {'text':chat}
