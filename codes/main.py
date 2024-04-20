456c7d35cdc3691d612bca5c73da26d376d46164
Alifthi <alifathi8008@gmail.com>
2024-04-20 18:27:59 +0330

    finetuning: main file changed to make the file compatible with Federated Learning

--- codes/main.py
+++ codes/main.py
@@ -2,7 +2,10 @@ import os
 os.environ["OMP_NUM_THREADS"] = "4"
 from utils.utils import utils
 from model.Model import Model
-utils=utils()
-model=Model(tokenizer=utils.tokenizer, trainDataset=utils.data)
-model.train()
-model.saveModel()
+class main:
+    def __init__(self,name) -> None:    
+        self.utils=utils(name)
+        self.model=Model(tokenizer=self.utils.tokenizer, trainDataset=self.utils.data, name=name)
+    def init(self):
+        self.model.train()
+        self.model.saveModel()
