9662a43b63cb0c3d5fe57fb7a2b7d44793b22615
Alifthi <alifathi8008@gmail.com>
2024-06-19 21:53:50 +0330

    finetuning: inference method developed.

--- codes/model/Model.py
+++ codes/model/Model.py
@@ -1,6 +1,6 @@
 import torch
 from transformers import AutoModelForCausalLM, BitsAndBytesConfig, TrainingArguments
-from peft import LoraConfig
+from peft import LoraConfig,PeftModel
 from trl import SFTTrainer
 from config import (MODEL_NAME, LORA_R, LORA_ALPHA,  
                     LORA_DROPOUT, TASK_TYPE, OUTPUTS_DIR, 
@@ -43,11 +43,32 @@ class Model:
             tokenizer=self.tokenizer,
             args=self.trainingAguments            
         )
-    def inference(self):
-        pass
+    def inference(self,modelChkpnt):
+        device = "cuda" if torch.cuda.is_available() else "cpu"  
+        self.model=PeftModel.from_pretrained(self.model,modelChkpnt,
+                                             torch_dtype=torch.float16,
+                                             device=device)
+        messages=[]
+        while True:
+            prompt=input("prompt: ")
+            messages.append({"role": "user", "content": prompt})
+            text = self.tokenizer.apply_chat_template(
+                messages,
+                tokenize=False,
+                add_generation_prompt=True)
+            model_inputs = self.tokenizer([text], return_tensors="pt").to(device)
+
+            generated_ids = self.model.generate(
+                model_inputs.input_ids,
+                max_new_tokens=512,num_beams=8,
+                num_return_sequences=1,top_k=200,
+                top_p=0.9,temperature=0.9)
+            generated_ids = [
+                output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)]
+            response = self.tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+            messages.append({"role": "assistant", "content": response})
+            print(response) 
     def loadModel(self):
         self.model=AutoModelForCausalLM.from_pretrained(MODEL_NAME,device_map='auto')
     def saveModel(self):
         self.model.save_pretrained(MODEL_PATH+self.modelName+'_'+self.name+'/')
-    def quantizationConfig(self):
-        pass
